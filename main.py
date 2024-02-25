a1d216419b28f6d95832f3681dd8a418bd43290b
maksim0840 <maksim0840@gmail.com>
2024-02-25 05:19:23 +0300

    users_info table

--- main.py
+++ main.py
@@ -11,34 +11,67 @@ client = Client(host=client_host,
 
 
 def create_new_user(ip, username="non-registered", password="non-registered"):
-	if (username == "non-registered" or password == "non-registered"):
+	# генерация нового UUID (Universally Unique Identifier)
+	user_id = client.execute('''
+		SELECT generateUUIDv4()
+		''')[0][0]
+
+	if ((username == "non-registered" or password == "non-registered")\):
 		client.execute('''
 			INSERT INTO users_info (* EXCEPT(username, password)) 
-			Values (generateUUIDv4(), '{0}', now64(3, 'Europe/Moscow'))
-		'''.format(ip))
+			Values ('{0}', '{1}', now64(3, 'Europe/Moscow'))
+			'''.format(user_id, ip))
 	else:
+		if (user_id_by_username(username) != None):
+			return None # пользователь с таким ником уже зарегестрирован
+
 		client.execute('''
 			INSERT INTO users_info (*) 
-			Values (generateUUIDv4(), '{0}', '{1}', '{2}', now64(3, 'Europe/Moscow'))
-		'''.format(ip, username, password))
+			Values ('{0}', '{1}', '{2}', '{3}', now64(3, 'Europe/Moscow'))
+			'''.format(user_id, ip, username, password))
+	return user_id
 
+def log_in_confirmation(username, password):
+	user_id = client.execute('''
+		SELECT user_id FROM users_info
+		WHERE (username = '{0}') AND (password = '{1}')
+		'''.format(username, password))
 
-create_new_user('100.253.40.133', 'стёпа', 'стёпа1015')
+	if (len(user_id) != 0):
+		return user_id[0][0]
+	return None
+
+def user_id_by_username(username):
+	user_id = client.execute('''
+		SELECT user_id FROM users_info
+		WHERE (username = '{0}')
+		'''.format(username))
+
+	if (len(user_id) != 0):
+		return user_id[0][0]
+	return None
+
+print(create_new_user('100.253.40.133'))
+#print(create_new_user('100.253.40.133', 'bob2', 'random1222321'))
+# print(create_new_user('100.253.40.133', 'asd', 'ads'))
+# print(create_new_user('100.253.40.212'))
+#print(log_in_confirmation("стёпа", "стёпа1015"))
 
 
 # client.execute('''DROP TABLE users_info''')
 # res = client.execute('''
 # 	CREATE TABLE users_info 
 # 	(
-# 		user_id UUID NOT NULL,
+# 		user_id UUID,
 # 		ip IPv4,
 # 		username String,
 # 		password String,
-# 		registration_date DateTime64(3, 'Europe/Moscow') NOT NULL
+# 		registration_date DateTime64(3, 'Europe/Moscow')
 # 	)
 # 	ENGINE = MergeTree()
 # 	PRIMARY KEY (user_id, username)
+
+# 	TTL toDateTime(registration_date) + INTERVAL 1 MONTH DELETE 
+# 	WHERE (empty(username) AND empty(password))
 # 	''')
 # print(res)
-
-# def 
