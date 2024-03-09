f166b90d188a6ab863454286c992b76a999d3a7d
maksim0840 <117035413+maksim0840@users.noreply.github.com>
2024-03-09 21:43:25 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,9 +2,7 @@ from ClientWrapper import *
 
 db = DB()
 
-# db.users_info.print(rows=2)
-# db.urls_info.print(rows=2)
-# db.visitors_info.print(rows=2)
+db.users_info.print(rows=2)
+db.urls_info.print(rows=2)
+db.visitors_info.print(rows=2)
 
-#db.users_info.clear()
-db.users_info.append_from_file("db_test_files/users_info.csv")
